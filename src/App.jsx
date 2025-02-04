c201848b283c5d15816ba09cb654de08edb1b606
Mohamed Elshaowky <elshoky330@gmail.com>
2025-02-04 17:02:29 +0200

    Use useContext insted of fetch twice

--- src/App.jsx
+++ src/App.jsx
@@ -6,35 +6,33 @@ import Hero from "./commponents/hero/Hero";
 import Main from "./commponents/main/Main";
 import Footer from "./commponents/footer/Footer";
 import ScrollTotop from "./commponents/scroll/ScrollTotop";
-import { useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Cart from "./commponents/main/Cart";
 // تعديل Router
 import PropTypes from "prop-types";
 import ProductDetails from "./commponents/main/ProductDetails";
 import LogForm from "./commponents/main/logForm";
-const MainPage = ({
-  Myproducts,
-  loading,
-  openCart,
-  toggleDrawerCart,
-  SearchValue,
-}) => (
-  <>
-    <Hero />
-    {loading ? (
-      <p>Loading...</p>
-    ) : (
-      <Main
-        Myproducts={Myproducts}
-        loading={loading}
-        openCart={openCart}
-        toggleDrawerCart={toggleDrawerCart}
-        SearchValue={SearchValue}
-      />
-    )}
-  </>
-);
+import ProductsContext, { ProductProvider } from "./context/ProductsContext";
+const MainPage = ({ openCart, toggleDrawerCart, SearchValue }) => {
+  const { Myproducts, loading } = useContext(ProductsContext);
+  return (
+    <>
+      <Hero />
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <Main
+          Myproducts={Myproducts}
+          loading={loading}
+          openCart={openCart}
+          toggleDrawerCart={toggleDrawerCart}
+          SearchValue={SearchValue}
+        />
+      )}
+    </>
+  );
+};
 // تعديل Router
 function App() {
   // Search
@@ -46,75 +44,57 @@ function App() {
   const toggleDrawerCart = (newOpen) => () => {
     setOpenCart(newOpen);
   };
-
-  const [Myproducts, setMyproducts] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("https://fakestoreapi.com/products");
-        const result = await response.json();
-        setMyproducts(result);
-        setLoading(false);
-      } catch (error) {
-        console.error("Error Fetching Data", error);
-        setLoading(false);
-      }
-    };
-    fetchData();
-  }, []);
   return (
-    <BrowserRouter>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          minHeight: "100vh", // يجعل الصفحة تأخذ الارتفاع بالكامل
-        }}
-      >
-        <ColorModeContext.Provider value={colorMode}>
-          <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <Box sx={{ flex: 1 }}>
-              <Header1 />
-              <Header2
-                toggleDrawerCart={toggleDrawerCart}
-                SearchValue={SearchValue}
-                setSearchValue={setSearchValue}
-              />
-              <Routes>
-                <Route
-                  path="/"
-                  element={
-                    <MainPage
-                      Myproducts={Myproducts}
-                      loading={loading}
-                      openCart={openCart}
-                      toggleDrawerCart={toggleDrawerCart}
-                      SearchValue={SearchValue}
-                    />
-                  }
+    <ProductProvider>
+      <BrowserRouter>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            minHeight: "100vh", // يجعل الصفحة تأخذ الارتفاع بالكامل
+          }}
+        >
+          <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+              <CssBaseline />
+              <Box sx={{ flex: 1 }}>
+                <Header1 />
+                <Header2
+                  toggleDrawerCart={toggleDrawerCart}
+                  SearchValue={SearchValue}
+                  setSearchValue={setSearchValue}
                 />
-                <Route
-                  path="/cart"
-                  element={
-                    <Cart
-                      toggleDrawerCart={toggleDrawerCart}
-                      openCart={openCart}
-                    />
-                  }
-                />
-                <Route path="/product/:id" element={<ProductDetails />} />
-                <Route path="/logform" element={<LogForm />} />
-              </Routes>
-            </Box>
-            <Footer />
-            <ScrollTotop />
-          </ThemeProvider>
-        </ColorModeContext.Provider>
-      </Box>
-    </BrowserRouter>
+                <Routes>
+                  <Route
+                    path="/"
+                    element={
+                      <MainPage
+                        openCart={openCart}
+                        toggleDrawerCart={toggleDrawerCart}
+                        SearchValue={SearchValue}
+                      />
+                    }
+                  />
+                  <Route
+                    path="/cart"
+                    element={
+                      <Cart
+                        toggleDrawerCart={toggleDrawerCart}
+                        openCart={openCart}
+                      />
+                    }
+                  />
+                  <Route path="/product/:id" element={<ProductDetails />} />
+                  <Route path="/logform" element={<LogForm />} />
+                </Routes>
+              </Box>
+              <Footer />
+              <ScrollTotop />
+            </ThemeProvider>
+          </ColorModeContext.Provider>
+        </Box>
+      </BrowserRouter>
+    </ProductProvider>
   );
 }
 // إضافة PropTypes للتحقق من صحة البيانات
